f4591cfde8f2fecfcb84e1ae6d215bccdcc5d78b
ThanhNhanNguyen <thanhnhan.b12002@gmail.com>
2022-09-25 06:57:11 +1000

    Beer

--- src/main/java/nhan/App.java
+++ src/main/java/nhan/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println(isDivisibleByFive(50));
+		System.out.println("Beer");
     }
 
     public static boolean isDivisibleByFive(int number) {
